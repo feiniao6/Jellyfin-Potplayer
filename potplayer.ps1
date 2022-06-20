41b81ad1f084b22739af6e7b871c8c9dfd691ce6
serenity666 <64485323+serenity666@users.noreply.github.com>
2022-06-20 18:29:35 +0800

    Update potplayer.ps1

--- potplayer.ps1
+++ potplayer.ps1
@@ -2,5 +2,7 @@ Add-Type -Assembly System.Web
 $path=$args[0]
 $path=$path -replace "potplayer://" , ""
 $path= [System.Web.HttpUtility]::UrlDecode($path)
+$path=$path -replace "/" , " "
+$path=$path -replace   "potplayer:  " , "potplayer://"
 echo $path
 & "C:\\Program Files\\DAUM\\PotPlayer\\PotPlayerMini64.exe" $path
